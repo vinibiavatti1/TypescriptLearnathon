e26ca0873e0bae36d96aca561681c2fa0106d095
Vinícius Biavatti <vinicius.biavatti@create.pt>
2023-02-17 15:53:08 +0000

    07

--- src/ts/07-form.ts
+++ src/ts/07-form.ts
@@ -1,4 +1,4 @@
-const form = {
+/*const form = {
   name: 'Jose',
   age: 10,
   arrivsalDate: new Date(),
@@ -14,6 +14,36 @@ const age = getProperty('age');
 const name = getProperty('name');
 
 function updateForm(key, value) {
+  form[key] = value;
+}
+
+updateForm('name', 'askdjsa');
+updateForm('age', 10);*/
+
+interface FormPayload {
+  name: string
+  age: number
+  arrivalDate: Date
+  departureDate: Date
+  hasCovidVaccine: boolean
+}
+
+const form: FormPayload = {
+  name: 'Jose',
+  age: 10,
+  arrivalDate: new Date(),
+  departureDate: new Date(),
+  hasCovidVaccine: false,
+};
+
+function getProperty<T extends keyof FormPayload>(key: string): FormPayload[T] {
+  return form[key];
+}
+
+const age = getProperty('age');
+const name_ = getProperty('name');
+
+function updateForm<T extends keyof FormPayload>(key: string, value: FormPayload[T]): void {
   form[key] = value;
 }
 
